9b87f328cb4099b93cb3ffb3e46bd3d5cc4d40ac
Balogun Abdulsamad <abdulsamadbalogun25@gmail.com>
2025-07-26 23:35:47 +0100

    feat(checkout): add delivery otp verification

--- resources/views/emails/order-otp.blade.php
+++ resources/views/emails/order-otp.blade.php
@@ -3,9 +3,9 @@
 
 Your One-Time Password (OTP) is **{{ $otp }}**
 
-This code will expire in a few minutes.
+This code will expire in a 30 minutes.
 
-Please do not share this OTP with anyone. It is required to verify your cash-on-delivery order.
+Please do not share this OTP with anyone. It is required to verify your delivery order.
 
 Thanks,<br>
 {{ config('app.name') }}
