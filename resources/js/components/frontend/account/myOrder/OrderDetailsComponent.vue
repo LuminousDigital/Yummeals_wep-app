733afe16c82e281616127e180d06bc813df71c1b
Abi Prospa <126681430+AbiProspa@users.noreply.github.com>
2025-08-14 17:50:26 +0100

    otp-handling

--- resources/js/components/frontend/account/myOrder/OrderDetailsComponent.vue
+++ resources/js/components/frontend/account/myOrder/OrderDetailsComponent.vue
@@ -19,7 +19,7 @@
                             </span>
                         </div>
 
-                        <div class="flex flex-wrap items-center gap-2 mb-5">
+                        <div class="flex flex-wrap items-center gap-2 mb-3">
                             <span v-if="parseInt(order.order_type) == parseInt(enums.orderTypeEnum.TAKEAWAY)"
                                 class="text-sm capitalize">{{ $t("label.takeaway_time") }} :</span>
                             <span v-else class="text-sm capitalize">{{ $t("label.delivery_time") }}:</span>
@@ -27,21 +27,18 @@
                                 {{ order.delivery_date }} {{ order.delivery_time }}
                             </span>
                         </div>
-                       <div v-if="order.otp && order.otp_expiry" class="lex flex-wrap items-center gap-2 mb-5">
+                       <div v-if="order.otp" class="flex flex-wrap items-center gap-2 mb-3">
                          <div
-                           class="inline-flex flex-col max-[320px]:items-start max-[320px]:gap-1 sm:flex-row sm:items-center sm:space-x-2 border border-gray-300 rounded-xl px-3 py-2 bg-white shadow-sm w-auto"
+                           class="inline-flex flex-col max-[320px]:items-start max-[320px]:gap-1 sm:flex-row sm:items-center sm:space-x-2 border border-gray-300 rounded-xl px-2 py-1 bg-white shadow-sm w-auto"
                          >
                            <div class="flex items-center gap-2">
-                             <span class="text-sm font-semibold text-gray-600">
+                             <span class="text-[14px] font-semibold text-gray-600">
                                {{ $t("label.otp") }}:
                              </span>
-                             <span class="text-lg font-bold text-orange-500 tracking-widest">
+                             <span class="text-[16px] font-bold text-orange-500 tracking-widest">
                                {{ order.otp }}
                              </span>
                            </div>
-                           <span class="text-xs text-gray-500 max-[320px]:mt-1 pr-2">
-                             {{ $t("label.expires") }}: {{ formatOtpExpiry(order.otp_expiry) }}
-                           </span>
                          </div>
                        </div>
                         <OrderStatusComponent :props="order" />
@@ -128,7 +125,8 @@
                         </a>
                     </div>
                 </div>
-                <!-- <div class="w-full rounded-2xl shadow-xs bg-white">
+
+                <div class="w-full rounded-2xl shadow-xs bg-white">
                     <div class="p-4 border-b">
                         <h3 class="font-medium text-sm leading-6 capitalize mb-4">{{ $t('label.order_details') }}</h3>
                         <div class="pl-3">
@@ -209,26 +207,218 @@
                             </div>
                         </div>
                     </div>
-                    <div class="p-4" v-if="order.status === enums.orderStatusEnum.PENDING">
+                    <!-- <div class="p-4" v-if="order.status === enums.orderStatusEnum.PENDING">
                         <button @click="changeStatus(enums.orderStatusEnum.CANCELED)"
-                            class="w-full rounded-3xl capitalize font-medium leading-6 py-3 text-white bg-[#FB4E4E]">
+                            class="w-full rounded-3xl capitalize font-medium leading-6 py-3 text-white bg-[#FB4E4E] mb-3">
                             {{ $t('label.cancel_order') }}
                         </button>
+                    </div> -->
+                    <div class="p-4">
+                        <button type="button" v-print="printObj"
+                            class="flex w-full items-center justify-center gap-2 px-4 py-3.5 rounded-full shadow-db-card bg-[#1AB759]">
+                            <i class="lab lab-printer-line text-base font-medium text-white"></i>
+                            <span class="text-base font-medium capitalize text-white"> Print Order Details</span>
+                        </button>
                     </div>
-                    <div class="p-4" v-else>
-                        <FrontendOrderReceiptComponent :order="order" :orderBranch="orderBranch"
-                            :orderItems="orderItems" :orderUser="orderUser" :orderAddress="orderAddress" />
-                    </div>
-
-
-                </div> -->
+                </div>
             </div>
         </div>
     </section>
+
+    <!-- Receipt Modal -->
+    <div id="receipt" class="modal">
+        <div class="modal-dialog max-w-[340px] rounded-none" id="print" :dir="direction">
+            <div class="modal-body">
+                <div class="text-center pb-3.5 border-b border-dashed border-gray-400">
+                    <h3 class="text-2xl font-bold mb-1">{{ setting.company_name }}</h3>
+                    <h4 class="text-sm font-normal">{{ orderBranch.address }}</h4>
+                    <h5 class="text-sm font-normal">Tel: {{ orderBranch.phone }}</h5>
+                </div>
+
+                <table class="w-full my-1.5">
+                    <tbody>
+                        <tr>
+                            <td class="text-xs text-left py-0.5 text-heading">{{ $t('button.order') }}
+                                #{{ order.order_serial_no }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-xs text-left py-0.5 text-heading">{{ order.order_date }}</td>
+                            <td class="text-xs text-right py-0.5 text-heading">{{ order.order_time }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table class="w-full">
+                    <thead class="border-t border-b border-dashed border-gray-400">
+                        <tr>
+                            <th scope="col" class="py-1 font-normal text-xs capitalize text-left text-heading w-8">
+                                {{ $t('label.qty') }}
+                            </th>
+                            <th scope="col"
+                                class="py-1 font-normal text-xs capitalize flex items-center justify-between text-heading">
+                                <span>{{ $t('label.item_description') }}</span>
+                                <span>{{ $t('label.price') }}</span>
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="border-b border-dashed border-gray-400">
+                        <tr v-if="orderItems.length > 0" v-for="item in orderItems" :key="item">
+                            <td class="text-left font-normal align-top py-1">
+                                <p class="text-xs leading-5 text-heading">{{ item.quantity }}</p>
+                            </td>
+                            <td class="text-left font-normal align-top py-1">
+                                <div class="flex items-center justify-between">
+                                    <h4 class="text-sm font-normal capitalize">{{ item.item_name }}</h4>
+                                    <p class="text-xs leading-5 text-heading">{{ item.total_without_tax_currency_price }}
+                                    </p>
+                                </div>
+                                <p v-if="Object.keys(item.item_variations).length !== 0"
+                                    class="text-xs leading-5 font-normal text-heading max-w-[200px]">
+                                    <span v-for="(variation, index) in item.item_variations">
+                                        {{ variation.variation_name }}: {{ variation.name }}
+                                        <span v-if="index + 1 < Object.keys(item.item_variations).length">, </span>
+                                    </span>
+                                </p>
+                                <p v-if="item.item_extras.length > 0"
+                                    class="text-xs leading-5 font-normal text-heading max-w-[200px]">
+                                    {{ $t('label.extras') }}:
+                                    <span v-for="(extra, index) in item.item_extras">
+                                        {{ extra.name }}
+                                        <span v-if="index + 1 < item.item_extras.length">, </span>
+                                    </span>
+                                </p>
+                                <p v-if="item.instruction" class="text-xs leading-5 font-normal text-heading max-w-[200px]">
+                                    {{ $t('label.instruction') }}: {{ item.instruction }}
+                                </p>
+
+                                <div class="flex items-center justify-between" v-if="item.tax_rate > 0">
+                                    <p class="text-xs leading-5 font-normal text-heading">{{ item.tax_name }}
+                                        ({{ item.tax_currency_rate }} {{ item.tax_type }})</p>
+                                    <p class="text-xs leading-5 font-normal text-heading">
+                                        {{ item.tax_currency_amount }}
+                                    </p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div class="py-2 pl-7">
+                    <table class="w-full">
+                        <tbody>
+                            <tr>
+                                <td class="text-xs text-left py-0.5 uppercase text-heading">{{ $t('label.subtotal') }}:</td>
+                                <td class="text-xs text-right py-0.5 text-heading">
+                                    {{ order.subtotal_without_tax_currency_price }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-xs text-left py-0.5 uppercase text-heading">
+                                    {{ $t('label.total_tax') }}:
+                                </td>
+                                <td class="text-xs text-right py-0.5 text-heading">
+                                    {{ order.total_tax_currency_price }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-xs text-left py-0.5 uppercase text-heading">{{ $t('label.discount') }}:</td>
+                                <td class="text-xs text-right py-0.5 text-heading">{{ order.discount_currency_price }}</td>
+                            </tr>
+
+                            <tr v-if="order.order_type === enums.orderTypeEnum.DELIVERY">
+                                <td class="text-xs text-left py-0.5 uppercase text-heading">{{ $t('label.delivery_charge')
+                                }}:
+                                </td>
+                                <td class="text-xs text-right py-0.5 text-heading">{{ order.delivery_charge_currency_price
+                                }}
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-xs text-left py-0.5 font-bold uppercase text-heading">
+                                    {{ $t('label.total') }}:
+                                </td>
+                                <td class="text-xs text-right py-0.5 font-bold text-heading">
+                                    {{ order.total_currency_price }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="text-xs py-1 border-t border-b border-dashed border-gray-400 text-heading">
+                    <table class="w-full">
+                        <tbody>
+                            <tr v-if="parseInt(order.source) === enums.sourceEnum.POS && order.cash_back_amount > 0">
+                                <td class="pt-1 pb-1 pr-1 align-top text-start">{{ $t('label.payment_type') }}: {{ enums.posPaymentMethodEnumArray[order.pos_payment_method] }}</td>
+                                <td class="pt-1 pb-1 text-end" v-if="order.cash_back_amount > 0">
+                                    <div>{{ $t('label.cash') }}: {{ order.pos_received_currency_amount }}</div>
+                                    <span>{{ $t('label.change') }} : {{ order.cash_back_currency_amount }}</span>
+                                </td>
+                            </tr>
+                            <tr v-else-if="parseInt(order.source) === enums.sourceEnum.POS">
+                                <td class="pt-1 pb-1 pr-1 align-top text-start">{{ $t('label.payment_type') }}: </td>
+                                <td class="pt-1 pb-1">{{ enums.posPaymentMethodEnumArray[order.pos_payment_method] }}</td>
+                            </tr>
+                            <tr v-else>
+                                <td class="pt-1 pb-1 pr-1">{{ $t('label.payment_type') }}:</td>
+                                <td v-if="order.transaction" class="pt-1 pb-1">{{ order.transaction.payment_method }}</td>
+                                <td v-else class="pt-1 pb-1">{{ enums.paymentTypeEnumArray[order.payment_method] }}</td>
+                            </tr>
+                            <tr>
+                                <td class="pt-1 pb-1 pr-1">{{ $t('label.order_type') }}:</td>
+                                <td class="pt-1 pb-1">{{ enums.orderTypeEnumArray[order.order_type] }}</td>
+                            </tr>
+                            <tr>
+                                <td class="pt-1 pb-1 pr-1">{{ $t('label.delivery_time') }}:</td>
+                                <td class="pt-1 pb-1">{{ order.delivery_date }} {{ order.delivery_time }}</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="text-xs py-1 border-b border-dashed border-gray-400 text-heading">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td class="pt-1 pb-1 pr-1">{{ $t('label.customer') }}:</td>
+                                <td class="pt-1 pb-1">{{ orderUser.name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="pt-1 pb-1 pr-1">{{ $t('label.phone') }}:</td>
+                                <td class="pt-1 pb-1">{{ orderUser.country_code + '' + orderUser.phone }}</td>
+                            </tr>
+                            <tr v-if="order.order_type === enums.orderTypeEnum.DELIVERY">
+                                <td class="pt-1 pb-1 pr-1">{{ $t('label.address') }}:</td>
+                                <td class="pt-1 pb-1">
+                                    {{ orderAddress.apartment ? orderAddress.apartment + ', ' : '' }}
+                                    {{ orderAddress.address }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="text-center pt-2 pb-4">
+                    <p class="text-[11px] leading-[14px] capitalize text-heading">{{ $t('message.thank_you') }}</p>
+                </div>
+
+                <div class="flex flex-col items-end">
+                    <h5 class="text-[8px] font-normal text-left w-[46px] leading-[10px]">
+                        {{ $t('label.powered_by') }}
+                    </h5>
+                    <h6 class="text-xs font-normal leading-4">{{ setting.company_name }}</h6>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-
+import print from "vue3-print-nb";
 import LoadingComponent from "../../../admin/components/LoadingComponent";
 import alertService from "../../../../services/alertService";
 import appService from "../../../../services/appService";
@@ -238,18 +428,25 @@ import paymentStatusEnum from "../../../../enums/modules/paymentStatusEnum";
 import paymentTypeEnum from "../../../../enums/modules/paymentTypeEnum";
 import OrderStatusComponent from "../../components/OrderStatusComponent";
 import OrderDetailsMapComponent from "./OrderDetailsMapComponent";
-import FrontendOrderReceiptComponent from "./FrontendOrderReceiptComponent";
 import activityEnum from "../../../../enums/modules/activityEnum";
 import sourceEnum from "../../../../enums/modules/sourceEnum";
 import posPaymentMethodEnum from "../../../../enums/modules/posPaymentMethodEnum";
+import displayModeEnum from "../../../../enums/modules/displayModeEnum";
 
 export default {
     name: "OrderDetailsComponent",
-    components: { OrderDetailsMapComponent, LoadingComponent, OrderStatusComponent, FrontendOrderReceiptComponent },
+    components: { OrderDetailsMapComponent, LoadingComponent, OrderStatusComponent },
+    directives: {
+        print
+    },
     data() {
         return {
             loading: {
                 isActive: false,
+            },
+            printObj: {
+                id: "print",
+                popTitle: this.$t("menu.order_receipt"),
             },
             enums: {
                 activityEnum: activityEnum,
@@ -312,6 +509,9 @@ export default {
         orderAddress: function () {
             return this.$store.getters['frontendOrder/orderAddress'];
         },
+        direction: function () {
+            return this.$store.getters['frontendLanguage/show'].display_mode === displayModeEnum.RTL ? 'rtl' : 'ltr';
+        },
     },
     mounted() {
         this.loading.isActive = true;
@@ -328,14 +528,14 @@ export default {
         orderStatusClass: function (status) {
             return appService.orderStatusClass(status);
         },
-        formatOtpExpiry: function (expiry) {
-            try {
-                const date = new Date(expiry);
-                return date.toLocaleString();
-            } catch (e) {
-                return expiry;
-            }
-        },
+        // formatOtpExpiry: function (c) {
+        //     try {
+        //         const date = new Date(expiry);
+        //         return date.toLocaleString();
+        //     } catch (e) {
+        //         return expiry;
+        //     }
+        // },
         changeStatus: function (status) {
             appService.cancelOrder().then((res) => {
                 try {
